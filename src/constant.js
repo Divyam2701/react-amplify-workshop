122b640039b392a838d5748d7921dafac596bed3
Divyam2701 <130549027+Divyam2701@users.noreply.github.com>
2025-07-26 12:27:57 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 // export const endpoint = 'http://15.206.164.156:8080';
-export const endpoint = 'https://avg0849e6a.execute-api.ap-south-1.amazonaws.com';
+export const endpoint = 'https://eh306cp32g.execute-api.ap-south-1.amazonaws.com';
 
 export const API_PATH = '/api/users';
